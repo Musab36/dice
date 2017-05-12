3562105ecf34277b8b5cf87ca05989105675561a
Musab36 <musabhd@gmail.com>
2017-05-12 17:51:33 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 function playerOne(){
     var die1 = document.getElementById("roll1");
     var die2 = document.getElementById("roll2");
-    var status = document.getElementById("ply1");
+    var ply1 = document.getElementById("ply1");
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
     var diceTotal = d1 + d2;
     die1.innerHTML = d1;
     die2.innerHTML = d2;
-    status.innerHTML = "You rolled "+diceTotal+".";
+    ply1.innerHTML = "You rolled "+diceTotal+".";
     if(d1 == d2){
         ply1.innerHTML += " You striked DOUBLES!! You won a free round!!";
     }
@@ -16,7 +16,7 @@ function playerOne(){
 function playerTwo(){
     var die1 = document.getElementById("roll3");
     var die2 = document.getElementById("roll4");
-    var status = document.getElementById("ply2");
+    var ply2 = document.getElementById("ply2");
     var d1 = Math.floor(Math.random() * 6) + 1;
     var d2 = Math.floor(Math.random() * 6) + 1;
     var diceTotal = d1 + d2;
